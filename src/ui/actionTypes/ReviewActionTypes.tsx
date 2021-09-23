7b2935f640394a4e6c5965d9eee173a4b4b1a1eb
Sunil <sunil.raghuwansh@gmail.com>
2021-09-23 14:02:26 +0530

    type script is done

--- src/ui/actionTypes/ReviewActionTypes.tsx
+++
@@ -1,1 +0,0 @@
-export const GET_REVIEW_DATA = "GET_REVIEW_DATA";
--- src/ui/actions/ReviewActions.tsx
+++
@@ -1,9 +0,0 @@
-import { Root } from '../../api/model/ReviewModel';
-import { GET_REVIEW_DATA } from '../actionTypes/ReviewActionTypes';
-
-export const getReviewData = (result:Root) => {
-    return {
-        type: GET_REVIEW_DATA,
-        payload: result
-    }
-}
